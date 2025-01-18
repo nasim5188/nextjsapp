2b3b1590077647e5d977c78801094ac3b8b176b8
nasim5188 <nasim.ahamed411@gmail.com>
2025-01-18 11:58:13 +0600

    Update script file

--- scripts/deployment-dev.sh
+++ scripts/deployment-dev.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if docker ps | grep -i nodejs-next-app then
-    docker stop nodejs-next-app
-    docker rm nodejs-next-app:latest
-    docker rmi cseahmed01/nodejs-next-app:latest
 
+if docker ps | grep -i nodejs-next-app; then
+    docker stop nodejs-next-app
+    docker rm nodejs-next-app
+    docker rmi cseahmed01/nodejs-next-app:latest
 fi
+
 docker pull cseahmed01/nodejs-next-app:latest
 docker run --name nodejs-next-app -d -p 80:3000 cseahmed01/nodejs-next-app
